42510e0e2ef3b9faedb24399aff0c4b80dbcc797
Edimar Dias dos Santos <97312864+Edimar-Dias-dos-Santos@users.noreply.github.com>
2022-10-05 19:47:41 -0300

    setUpProjeto

---
+++ src/main/java/com/API/CentroPokemon/CentroPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.API.CentroPokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroPokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/API/CentroPokemon/CentroPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.CentroPokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
